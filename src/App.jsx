f35930dc516e72273c56955e84d03fed39f06a48
Dustin-Davis <drpookydavis@comcast.net>
2022-11-25 18:52:01 -0800

    moved my fetch to an api file for better file management

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,16 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import axios from "axios";
+import {fetchItems} from './api.jsx'
 
-const fetchItems = () => {
- return axios.get(`https://botw-compendium.herokuapp.com/api/v2/category/equipment`)
-    .then(({ data }) => {
-      console.log(data)
-      return data
-    })
-}
-
-  //I want to test to see if i can commit
 
 function App() {
-  const [count, setCount] = useState(0)
   const [items, setItems] = useState([])
   const [itemData, setItemData] = useState('')
 
   useEffect(() => {
     fetchItems().then((equipment) => {
-      setItemData(JSON.stringify(equipment) || 'No items')
+      // setItemData(JSON.stringify(equipment) || 'No items')
       setItems(equipment.data)
     })
   }, [])
@@ -29,19 +20,17 @@ function App() {
   return (
     <div className="App">
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         {
           items.sort((a, b) => a.id - b.id)
           .map((item, idx) => (
-            <p key={item.id}>
+            <div key={item.id}>
+            <p >
             {item.name}
           </p>
-        ))
-      }
+            </div>
+          ))
+        }
       </div>
-
     </div>
   )
 }
--- src/api.jsx
+++ src/api.jsx
@@ -1,3 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+
+
+export const fetchItems = () => {
+  return axios.get(`https://botw-compendium.herokuapp.com/api/v2/category/equipment`)
+    .then(({ data }) => {
+      console.log(data)
+      return data
+    })
+}
