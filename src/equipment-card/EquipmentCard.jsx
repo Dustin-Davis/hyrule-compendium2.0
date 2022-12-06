5bb9f8f193edc39737706dd5c1bad949afa13c0d
Dustin-Davis <drpookydavis@comcast.net>
2022-12-06 15:44:23 -0800

    moved a tag to top of page

--- src/equipment-card/EquipmentCard.jsx
+++ src/equipment-card/EquipmentCard.jsx
@@ -10,30 +10,32 @@ export default function EquipmentCard() {
   let num = 16
 
   return (
-    <div className="container">
-      <div className="row">
-        <a href='#monster'>monster</a>
-      {
-        equipment.sort((a, b) => a.id - b.id)
-        .map((item, idx) => idx < num && (
-          <div key={item.id} className="column">
-              <div className="flip-card" tabIndex="0">
-                <div className="flip-card-inner">
-                  <div className="flip-card-front">
-                    <img src={item.image} alt=""></img>
-                  </div>
-                  <div className="flip-card-back">
-                    <p>{item.name.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())}</p>
-                    <p>{item.description}</p>
-                    <p>Attack: {item.attack}</p>
-                    <p>Defense: {item.defense}</p>
+    <>
+      <a href='#monster'>monster</a>
+      <div className="container">
+        <div className="row">
+        {
+          equipment.sort((a, b) => a.id - b.id)
+          .map((item, idx) => idx < num && (
+            <div key={item.id} className="column">
+                <div className="flip-card" tabIndex="0">
+                  <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                      <img src={item.image} alt=""></img>
+                    </div>
+                    <div className="flip-card-back">
+                      <p>{item.name.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())}</p>
+                      <p>{item.description}</p>
+                      <p>Attack: {item.attack}</p>
+                      <p>Defense: {item.defense}</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-          </div>
-          ))
-        }
-      </div>
-  </div>
+            </div>
+            ))
+          }
+        </div>
+    </div>
+  </>
   )
 }
