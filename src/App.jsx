a3f221abee88dc7e9cf4f1d6224ab249da3c6f24
Dustin-Davis <drpookydavis@comcast.net>
2022-12-05 16:11:29 -0800

    added a card for monsters

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import axios from "axios";
 import EquipmentCard from './equipment-card/EquipmentCard.jsx'
+import MonsterCard from './monster-card/MonsterCard.jsx';
 
 function App() {
 
   return (
-    <div className="App">
-      <EquipmentCard />
+    <div className="App row">
+      <div>
+        <EquipmentCard />
+      </div>
+      <div >
+        <MonsterCard />
+      </div>
     </div>
   )
 }
