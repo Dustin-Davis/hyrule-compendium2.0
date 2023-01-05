06183cb0ba3c8cd66bccb8e19c61ed81005aaa55
Dustin-Davis <drpookydavis@comcast.net>
2023-01-05 13:35:03 -0800

    added changes to monster card

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -6,16 +6,16 @@ export default function NavBar() {
 
   return (
   <>
-    <nav class="navbar">
-      <div class="left">
+    <nav className="navbar">
+        <div className="left">
         <h1>Navbar</h1>
       </div>
-      <div class="right">
+        <div className="right">
         <input type="checkbox" id="check" />
-          <label for="check" class="checkBtn">
-            <i class="fa fa-bars"></i>
+          <label for="check" className="checkBtn">
+            <i className="fa fa-bars"></i>
           </label>
-          <ul class="list">
+          <ul className="list">
             <li><a href="#">Creatures</a></li>
             <li><a href="#">Equipment</a></li>
             <li><a href="#">Materials</a></li>
