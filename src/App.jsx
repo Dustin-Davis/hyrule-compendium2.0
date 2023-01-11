e8c57b2cd4439c103d89119894bb110f955382dd
Dustin-Davis <drpookydavis@comcast.net>
2023-01-11 14:13:35 -0800

    added a new style of card to monster-card

--- src/App.jsx
+++ src/App.jsx
@@ -4,42 +4,13 @@ import './App.css'
 import axios from "axios";
 import parseRoute from './lib/parse-route.js'
 import EquipmentCard from './equipment-card/EquipmentCard.jsx'
-// import MonsterCard from './monster-card/MonsterCard.jsx';
+import MonsterCard from './monster-card/MonsterCard.jsx';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      route: parseRoute(window.location.hash)
-    };
-  }
+export default function App() {
 
-  componentDidMount() {
-    window.addEventListener('hashchange', event => {
-      this.setState({ route: parseRoute(window.location.hash) });
-    });
-  }
+  return(
+    // <EquipmentCard />
+    <MonsterCard />
+  )
 
-  renderPage() {
-    const { route } = this.state;
-    if (route.path === '') {
-      return <EquipmentCard />;
-    }
-    // if (route.path === 'monster') {
-    //   return <MonsterCard />;
-    // }
-    // if (route.path === 'sign-up') {
-    //   return <SignUp />;
-    // }
-    // return <NotFound />;
-  }
-
-  render() {
-    const { route } = this.state;
-    return (
-        <>
-          {this.renderPage()}
-        </>
-    );
-  }
 }
--- src/monster-card/MonsterCard.jsx
+++ src/monster-card/MonsterCard.jsx
@@ -1,12 +1,26 @@
-// import React from "react";
-// import './MonsterCard.css'
-// import { useMonsters } from '../api.jsx'
+import React from "react";
+import './MonsterCard.css'
+import { useMonsters } from '../api.jsx'
 
 
 
 
-// export default function MonsterCard() {
-//   const monster = useMonsters()
-//   return (
-//   )
-// }
+export default function MonsterCard() {
+  const monster = useMonsters()
+  return (
+    <>
+      {
+        monster.sort((a, b) => a.id - b.id)
+          .map((item) => (
+            <div className="card Card" key={item.id}>
+              <img src={item.image} className="card-img-top" alt="..."></img>
+              <div className="card-body">
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          ))
+      }
+
+    </>
+  )
+}
