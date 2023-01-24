68b8428c9e542233939713b652eb8d5a75ab7785
Dustin-Davis <drpookydavis@comcast.net>
2023-01-24 12:29:46 -0800

    made the navbar the width of the page

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function NavBar() {
   return(
-    <div className="container">
+
     <nav className="navbar navbar-expand-lg bg-body-tertiary background-c">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">Navbar</a>
@@ -19,6 +19,6 @@ export default function NavBar() {
         </div>
       </div>
     </nav>
-    </div>
+
   )
 }
