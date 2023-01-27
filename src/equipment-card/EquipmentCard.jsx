f0020d67bed8e4a568cf8869a484782d3ecd6344
Dustin-Davis <drpookydavis@comcast.net>
2023-01-26 16:25:07 -0800

    added margin and padding

--- src/equipment-card/EquipmentCard.jsx
+++ src/equipment-card/EquipmentCard.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { useEquipment } from '../api.jsx'
-import './EquipmentCard.css'
+import "./EquipmentCard.css"
 
 
 export default function EquipmentCard() {
   const equipment = useEquipment()
-
+  let num = 8;
   return (
     <>
-          <div className="container">
-            <div className="row">
+      <div className="container py-5 text-center ">
+        <div className="row">
       {
         equipment.sort((a, b) => a.id - b.id)
-          .map((item) =>(
-              <div className="card Card "  key={item.id}>
+          .map((item, idx) => idx < num && (
+              <div className="card Card mb-3 mx-3"  key={item.id}>
                 <img src={item.image} className="card-img-top" alt="..."></img>
                 <div className="card-body">
                   <a href="#" className="btn btn-primary">Details</a>
