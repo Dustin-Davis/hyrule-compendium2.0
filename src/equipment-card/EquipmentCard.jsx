4a8d4b10579c118110eb89fedebee19549a6dcba
Dustin-Davis <drpookydavis@comcast.net>
2023-01-27 16:02:54 -0800

    center the card on mobile view

--- src/equipment-card/EquipmentCard.jsx
+++ src/equipment-card/EquipmentCard.jsx
@@ -5,16 +5,16 @@ import "./EquipmentCard.css"
 
 export default function EquipmentCard() {
   const equipment = useEquipment()
-  let num = 8;
+  let num = 6;
   return (
     <>
-      <div className="container py-5 text-center ">
-        <div className="row">
+      <div className="container py-5 text-center d-flex justify-content-around ">
+        <div className="row test">
       {
         equipment.sort((a, b) => a.id - b.id)
           .map((item, idx) => idx < num && (
-              <div className="card Card mb-3 mx-3"  key={item.id}>
-                <img src={item.image} className="card-img-top" alt="..."></img>
+              <div className="card Card mb-3 mx-3 col-3"  key={item.id}>
+                <img src={item.image} className="card-img-top img-size" alt="..."></img>
                 <div className="card-body">
                   <a href="#" className="btn btn-primary">Details</a>
                 </div>
