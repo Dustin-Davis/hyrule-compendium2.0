0c455dbfc62ffe40795816dd586761859400f67e
Dustin-Davis <drpookydavis@comcast.net>
2023-04-03 13:28:57 -0700

    optimized my code to add a capitalize function

---
+++ src/capitalize.jsx
@@ -0,0 +1,7 @@
+export default function capitalizeFirstLetters(str) {
+  let words = str.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+  return words.join(' ');
+}
